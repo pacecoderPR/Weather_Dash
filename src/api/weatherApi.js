e04c99bbcb4e10f13bdb39ca85ac81afd152d8c4
pritesh <priteshrajpurohit85@gmail.com>
2024-12-03 13:22:42 +0530

    Api key security issue fixed

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "24732e83c4171f0ce867e3a7f6a50134";
+const API_KEY = process.env.REACT_APP_OPENWEATHER_API_KEY;
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 export const getCurrentWeather = (city, unit = "metric") =>
